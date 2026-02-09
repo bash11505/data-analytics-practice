28c4562716e7faba76a110c19fb6d8a0650b823d
basha <shaikbasha11512@gmail.com>
2026-02-09 20:02:55 +0530

    Add files via upload

---
+++ day9.py
@@ -0,0 +1,20 @@
+#numpys
+import numpy as np 
+arr = np.array([10, 20, 30, 40])
+arr2 = np.array([[1, 2, 3], [4, 5, 6]])
+arr3 = np.array(["apple", "banana", "cherry"])
+arr4 = np.array([1, 2, 3, 4], dtype=float)
+print(arr)
+print(arr2)
+print(arr3)
+print(arr4)
+#2nd programme
+import numpy as np 
+list= [5,6,7,8]
+arr=np.asarray(list)
+print(arr)
+#3rd programme
+import numpy as np 
+tupple =(9,10,2,6)
+arr=np.asarray(tupple)
+print(arr)
