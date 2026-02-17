baa453ad14816ef017ae6e0ae3dad733f9b786bb
basha <shaikbasha11512@gmail.com>
2026-02-17 11:53:52 +0530

    Add files via upload

---
+++ day12.py
@@ -0,0 +1,16 @@
+#arrange
+import numpy as np
+r=np.arange(0,10,2)
+print(r)
+#np.linspace
+import numpy as np 
+l=np.linspace(0,1,5)
+print(l)
+#np.logspace
+import numpy as np 
+b=np.logspace(0,2,2)
+print(b)
+#np eye
+import numpy as np 
+e=np.eye(6)
+print(e)
