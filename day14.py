30063913b8139a195588b6d4cf5a92ebc4ea7640
basha <shaikbasha11512@gmail.com>
2026-02-19 20:02:01 +0530

    Add files via upload

---
+++ day14.py
@@ -0,0 +1,23 @@
+#1st programme
+import numpy  as np 
+a = np.random.rand()
+print(a)
+#2nd programme
+a = np.random.rand(3)
+print(a)
+#3th programme
+import numpy as np 
+a = np.random.randint(10)
+print(a)
+#4th programme
+import numpy as np 
+a = np.random.randint(1,10)
+print(a)
+#5th programme
+import numpy as np 
+a = np.random.randn()
+print(a)
+#6th programme
+import numpy as np 
+b=np.random.randn(3)
+print(b)
