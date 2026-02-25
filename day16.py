1aa89ba392dab4559c74b530872baf3041d51100
basha <shaikbasha11512@gmail.com>
2026-02-25 10:31:40 +0530

    Add files via upload

---
+++ day16.py
@@ -0,0 +1,32 @@
+#np.full
+import numpy as np 
+arr = np.full((5,5),-1)
+print(arr)
+#2nd problem 
+import numpy as np 
+arr=np.full((2,3),6)
+print(arr)
+#np.arrange
+import numpy as np 
+arr=np.arange(10)
+print(arr)
+#4th prblem 
+import numpy as np 
+arr = np.arange(5,66,2)
+print(arr)
+#np.linspace
+import numpy as np 
+arr=np.linspace(0,1,5)
+print(arr)
+#6th problem
+import numpy as np 
+arr=np.linspace(-1,1,5)
+print(arr)
+#np.log space
+import numpy as np 
+arr =np.logspace(0,2,5)
+print(arr)
+#8th problem 
+import numpy as np 
+arr = np.logspace(1,4,4)
+print(arr)
