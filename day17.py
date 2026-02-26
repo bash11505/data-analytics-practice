442cf8237c866e9d144eb06b5816962c6eb35628
basha <shaikbasha11512@gmail.com>
2026-02-26 16:54:16 +0530

    Add files via upload

---
+++ day17.py
@@ -0,0 +1,42 @@
+#1st programme
+import numpy as np 
+a=np.array([1,2,3,4])
+print(a)
+#2nd programme
+import numpy as np 
+b=np.array([[1,2],[3,4]])
+print(b)
+#3rd programme
+import numpy as np 
+x=[10,20,30,40]
+arr=np.asarray(x)
+print(arr)
+#4th programme
+import numpy as np 
+t=(5,6,7)
+arr=np.asarray(t)
+print(arr)
+#5th programme
+import numpy as np 
+s=np.zeros(5)
+print(s)
+#6th programme
+import numpy as np 
+z=np.zeros((2,3))
+print(z)
+#7th programme
+import numpy as np 
+arr=np.ones(4)
+print(arr)
+#8th programme
+import numpy as np 
+arr=np.ones((3,4))
+print(arr)
+#9th programme
+import numpy as np 
+arr=np.empty(5)
+print(arr)
+#10th programme
+import numpy as np 
+arr=np.empty((2,3))
+print(arr)
