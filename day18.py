d766cc8e888a982c9ae24e969086e0919b7cc420
basha <shaikbasha11512@gmail.com>
2026-02-27 11:31:24 +0530

    Add files via upload

---
+++ day18.py
@@ -0,0 +1,30 @@
+#arr.ndim
+import numpy as np 
+arr=np.array([1,2,3,4])
+print(arr.ndim)
+#2nd problem
+import numpy as np 
+arr=np.array([[1,2,3],
+            [4,5,6]])
+print(arr.ndim)
+#arr shape
+import numpy as np 
+arr=np.array([10,20,30,40])
+print(arr.shape)
+#4th probelm 
+import numpy as np 
+arr = np.array([[1,2],
+                [3,4],
+                [5,6]])
+print(arr.shape)
+#arr.size
+import numpy as np 
+arr=np.array([[2,3,4,5]
+                ,[6,7,8,9]])
+print(arr.size)
+#6th problem 
+import numpy as np 
+arr=np.array([[1,2,3]
+                ,[4,5,6]
+                ,[7,8,9]])
+[print(arr.size)]
