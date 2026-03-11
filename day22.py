fadf5e0775e02be6ab91fd12487c725c15718dea
basha <shaikbasha11512@gmail.com>
2026-03-11 21:17:46 +0530

    Add files via upload

---
+++ day22.py
@@ -0,0 +1,30 @@
+#np.add
+import numpy as np
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+result = np.add(a, b)
+print(result)
+#np.substract
+import numpy as np
+a = np.array([10, 20, 30])
+b = np.array([1, 2, 3])
+result = np.subtract(a, b)
+print(result)
+#np.multiply
+import numpy as np
+a = np.array([2, 3, 4])
+b = np.array([5, 6, 7])
+result = np.multiply(a, b)
+print(result)
+#np.divide
+import numpy as np
+a = np.array([10, 20, 30])
+b = np.array([2, 5, 3])
+result = np.divide(a, b)
+print(result)
+#np.power
+import numpy as np
+a = np.array([2, 3, 4])
+b = np.array([3, 2, 2])
+result = np.power(a, b)
+print(result)
