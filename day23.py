65eb6e883308148d23d2f418c5ccaf4a33f28b8c
basha <shaikbasha11512@gmail.com>
2026-03-23 18:53:56 +0530

    Add files via upload

---
+++ day23.py
@@ -0,0 +1,27 @@
+#np.Mod
+import numpy as np 
+print(np.mod(10,3))
+print(np.mod([10,20,30],7))
+#np.abs
+import numpy as np 
+print(np.abs(-5))
+print(np.abs([-1,-2,-3]))
+#np.Sign()
+import numpy as np 
+print(np.sign(-5))
+print(np.sign([3,4]))
+#np.round
+import numpy as np 
+print(np.round(3.6))       
+print(np.round(3.2))        
+print(np.round(3.456, 2))   
+#np floor
+import numpy as np 
+print(np.round(3.6))        
+print(np.round(3.2))        
+print(np.round(3.456, 2))   
+#np.ceil
+import numpy as np 
+print(np.ceil(3.2))
+print(np.ceil(3.6))
+print(np.ceil(3,456))
