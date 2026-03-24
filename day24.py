d043dcc40aa6e8720ebe5313c541f50afadac43e
basha <shaikbasha11512@gmail.com>
2026-03-24 19:50:11 +0530

    Add files via upload

---
+++ day24.py
@@ -0,0 +1,17 @@
+#np sum
+import numpy as np 
+arr=np.array([[1,2],[3,4]])
+print(np.sum(arr,axis=0))
+print(np.sum(arr,axis=1))
+#npmean
+import numpy as np 
+arr=np.array([[1,2,3,4]])
+print(np.mean(arr))
+#np.median
+import numpy as np 
+arr=np.array([1,2,3,4,5])
+print(np.median(arr))
+#np.average
+import numpy as np 
+arr=np.array([1,2,3,4,5,6,7,8,9,10])
+print(np.average(arr))
